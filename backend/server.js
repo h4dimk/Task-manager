d24f318d2e50e279de09aab8204089b8cabd3d2f
Hadi mk <hadimk04@gmail.com>
2024-07-10 10:09:20 +0530

    small change

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
-import multer from "multer";
 import db from "./config/db.js";
 import taskRoutes from "./routes/taskRoutes.js";
 import dotenv from "dotenv";
@@ -15,21 +13,11 @@ const corsOptions = {
   origin: process.env.CLIENT,
   credentials: true,
 };
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/uploads", express.static("uploads"));
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "uploads/");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + path.extname(file.originalname));
-//   },
-// });
-// const upload = multer({ storage });
 
 app.use("/tasks", taskRoutes);
 
